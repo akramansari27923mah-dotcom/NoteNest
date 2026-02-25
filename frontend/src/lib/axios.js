4c7fa5c55905787d5aae4d59b18f3e9956690174
Akram Ansari <akramansari27923mah@gmail.com>
2026-02-25 05:18:29 -0500

    fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3001/api' : 'https://notenest-wnaq.onrender.com/api'
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3001/api' : '/api'
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
