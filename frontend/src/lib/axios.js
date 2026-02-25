cea41f9cf298ad14c3b0383df208af7d16163f4d
Akram Ansari <akramansari27923mah@gmail.com>
2026-02-25 05:36:13 -0500

    fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3001/api' : '/api'
+const BASE_URL =
+    import.meta.env.MODE === 'development'
+        ? 'http://localhost:3001/api'
+        : import.meta.env.VITE_API_URL + '/api'
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
